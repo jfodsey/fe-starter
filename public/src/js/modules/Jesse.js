1343e75f3e45024f2ac31b81f8b9701cd5afba1d
Jesse Odsey <jfodsey@gmail.com>
2022-02-17 15:46:07 +0800

    "added url on json"

--- public/src/js/modules/Jesse.js
+++ public/src/js/modules/Jesse.js
@@ -1,6 +1,6 @@
 
 async function get_json(){
-    return fetch('/api/dashboard')
+    return fetch(window.location.href+'/api/dashboard')
     .then((response)=>response.json())
     .then((responseJson)=>{return responseJson});
 }
