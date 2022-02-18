eb93f20ca5c8d95fedcec0b64aad50aec077d3e5
Jesse Odsey <jfodsey@gmail.com>
2022-02-18 13:11:56 +0800

    revised Jesse.scss for UI

--- public/src/js/modules/Jesse.js
+++ public/src/js/modules/Jesse.js
@@ -1,6 +1,6 @@
 
 async function get_json(){
-    return fetch(window.location.href+'/api/dashboard')
+    return fetch('/api/dashboard')
     .then((response)=>response.json())
     .then((responseJson)=>{return responseJson});
 }
